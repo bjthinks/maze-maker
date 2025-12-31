42d1484d461e46f55aca2741e79dbf0f4a21e719
Brian Johnson <bjthinks@gmail.com>
2025-12-31 14:33:47 -0800

    Combine UnionM & Writer monads

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Control.Monad.Union
+import Control.Monad.Writer
+
+type Maze = WriterT String (UnionM (Int,Int))
+
+nl :: Maze ()
+nl = tell "\n"
+
+line :: String -> Maze ()
+line s = tell s >> nl
+
+test :: Maze ()
+test = do
+  line "testing union find monad"
+  return ()
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStr $ snd $ run $ runWriterT test
