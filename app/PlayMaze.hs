b08d2489c7078ae3740eed8787f94943201b6fd3
Brian Johnson <bjthinks@gmail.com>
2026-01-02 20:36:36 -0800

    Use StateT & ReaderT when playing the maze

--- app/PlayMaze.hs
+++ app/PlayMaze.hs
@@ -1,17 +1,26 @@
 module PlayMaze (playMaze) where
 
 import Control.Exception (bracket)
+import Control.Monad.Reader
+import Control.Monad.State
 import Data.Array
 import Graphics.Vty
 import Graphics.Vty.CrossPlatform
 
 type Maze = Array (Int,Int) Char
+type Op = StateT (Int,Int) (ReaderT (Vty,Maze) IO) -- state is player's (y,x)
 
 playMaze :: Maze -> IO String
 playMaze maze = bracket (mkVty defaultConfig) shutdown $ playMaze' maze
 
 playMaze' :: Maze -> Vty -> IO String
-playMaze' maze vty = eventLoop vty maze (1,0)
+playMaze' maze vty = runReaderT (evalStateT eventLoop (1,0)) (vty,maze)
+
+getVty :: Op Vty
+getVty = fst <$> ask
+
+getMaze :: Op Maze
+getMaze = snd <$> ask
 
 style :: Attr
 style = defAttr `withForeColor` white `withBackColor` black
@@ -23,57 +32,72 @@ basePicture maze =
       infoRow = "wasd or hjkl to move, q or ESC to quit"
   in picForImage $ foldr1 (<->) $ map (string style) (mazeRows ++ [infoRow])
 
-eventLoop :: Vty -> Maze -> (Int,Int) -> IO String
-eventLoop vty maze (y,x) = do
+eventLoop :: Op String
+eventLoop = do
+  maze <- getMaze
+  (y,x) <- get
   let ((_,_),(ymax,xmax)) = bounds maze
   if y == ymax-1 && x == xmax
     then return "Congratulations! You won!\n"
     else do
     let playerImage = translate x y (char style '@')
         composition = addToTop (basePicture maze) playerImage
-    update vty composition
-    e <- nextEvent vty
+    vty <- getVty
+    liftIO $ update vty composition
+    e <- liftIO $ nextEvent vty
     case e of
       EvKey (KChar 'q') [] -> return ""
       EvKey KEsc [] -> return ""
-      EvKey (KChar 'h') [] -> goLeft vty maze (y,x)
-      EvKey (KChar 'l') [] -> goRight vty maze (y,x)
-      EvKey (KChar 'k') [] -> goUp vty maze (y,x)
-      EvKey (KChar 'j') [] -> goDown vty maze (y,x)
-      EvKey (KChar 'w') [] -> goUp vty maze (y,x)
-      EvKey (KChar 'a') [] -> goLeft vty maze (y,x)
-      EvKey (KChar 's') [] -> goDown vty maze (y,x)
-      EvKey (KChar 'd') [] -> goRight vty maze (y,x)
-      EvKey KUp    [] -> goUp vty maze (y,x)
-      EvKey KLeft  [] -> goLeft vty maze (y,x)
-      EvKey KDown  [] -> goDown vty maze (y,x)
-      EvKey KRight [] -> goRight vty maze (y,x)
-      _ -> eventLoop vty maze (y,x)
+      EvKey (KChar 'h') [] -> goLeft
+      EvKey (KChar 'l') [] -> goRight
+      EvKey (KChar 'k') [] -> goUp
+      EvKey (KChar 'j') [] -> goDown
+      EvKey (KChar 'w') [] -> goUp
+      EvKey (KChar 'a') [] -> goLeft
+      EvKey (KChar 's') [] -> goDown
+      EvKey (KChar 'd') [] -> goRight
+      EvKey KUp    [] -> goUp
+      EvKey KLeft  [] -> goLeft
+      EvKey KDown  [] -> goDown
+      EvKey KRight [] -> goRight
+      _ -> eventLoop
 
-goLeft :: Vty -> Maze -> (Int,Int) -> IO String
-goLeft vty maze (y,x) = do
+goLeft :: Op String
+goLeft = do
+  maze <- getMaze
+  (y,x) <- get
   let ((_,xmin),(_,_)) = bounds maze
       x' = if x > xmin then x-1 else x
       c = maze ! (y,x')
-  eventLoop vty maze $ if c == ' ' then (y,x') else (y,x)
+  put $ if c == ' ' then (y,x') else (y,x)
+  eventLoop
 
-goRight :: Vty -> Maze -> (Int,Int) -> IO String
-goRight vty maze (y,x) = do
+goRight :: Op String
+goRight = do
+  maze <- getMaze
+  (y,x) <- get
   let ((_,_),(_,xmax)) = bounds maze
       x' = if x < xmax then x+1 else x
       c = maze ! (y,x')
-  eventLoop vty maze $ if c == ' ' then (y,x') else (y,x)
+  put $ if c == ' ' then (y,x') else (y,x)
+  eventLoop
 
-goUp :: Vty -> Maze -> (Int,Int) -> IO String
-goUp vty maze (y,x) = do
+goUp :: Op String
+goUp = do
+  maze <- getMaze
+  (y,x) <- get
   let ((ymin,_),(_,_)) = bounds maze
       y' = if y > ymin then y-1 else y
       c = maze ! (y',x)
-  eventLoop vty maze $ if c == ' ' then (y',x) else (y,x)
+  put $ if c == ' ' then (y',x) else (y,x)
+  eventLoop
 
-goDown :: Vty -> Maze -> (Int,Int) -> IO String
-goDown vty maze (y,x) = do
+goDown :: Op String
+goDown = do
+  maze <- getMaze
+  (y,x) <- get
   let ((_,_),(ymax,_)) = bounds maze
       y' = if y < ymax then y+1 else y
       c = maze ! (y',x)
-  eventLoop vty maze $ if c == ' ' then (y',x) else (y,x)
+  put $ if c == ' ' then (y',x) else (y,x)
+  eventLoop
